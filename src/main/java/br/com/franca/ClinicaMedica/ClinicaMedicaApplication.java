e68614047cf35ab142aa4ffda2a92b1cdb2c6212
Tiago França <tiagofranca.rita@gmail.com>
2024-05-17 09:12:34 -0300

    initial commit

---
+++ src/main/java/br/com/franca/ClinicaMedica/ClinicaMedicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.franca.ClinicaMedica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaMedicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaMedicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/franca/ClinicaMedica/ClinicaMedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.franca.ClinicaMedica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaMedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
